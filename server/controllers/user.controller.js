df06da4dfafa067cb6185fe967b3670c236f7653
Prasannaniroula <Prasannaniroula5@gmail.com>
2025-08-09 22:42:04 +0545

    implement user registration logic with email existence check and success response

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,5 @@
+import { User } from "../models/user.models.js"
+
 const home = async (req,res)=>{
     try {
             res
@@ -9,14 +11,25 @@ const home = async (req,res)=>{
     }
 }
 
-const register = (req,res)=>{
+const register = async (req,res)=>{
     try {
+        const {username,email,password}= req.body;
+        const userExists = await User.findOne({email});
+
+        if(userExists){
+            return res.status(400).json({msg:"email already exists"});
+        }
+
+        await User.create({username,email,password})
+        console.log("User registered successfully")
+
         res
         .status(200)
         .json({ message: req.body });
-    console.log(req.body);
+        console.log(req.body);
 } 
 catch (error) {
+    console.log("Couldnot update data in the database")
      console.log(error.message);    
 }
 

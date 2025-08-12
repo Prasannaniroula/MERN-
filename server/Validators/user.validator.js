a02aa9dc4bd0a6355c758be1a8ed662fb2a8d7f8
Prasannaniroula <Prasannaniroula5@gmail.com>
2025-08-12 09:41:07 +0545

    add Zod validation for user registration and integrate with middleware

---
+++ server/Validators/user.validator.js
@@ -0,0 +1,22 @@
+import {z} from "zod";
+
+const signupSchema = z.object({
+username: z
+    .string({required_error: "Name is required"})
+    .trim()
+    .min(3,{message:"Name must be atleast of 3 chars."})
+    .max(255,{message:"Name must not be more than 255 character"}),
+email: z
+    .string({required_error: "Email is required"})
+    .trim()
+    .email({message:"Invalid Email address"})
+    .min(3,{message:"Email must be atleast of 3 chars."})
+    .max(255,{message:"Email must not be more than 255 character"}),
+password: z
+    .string({required_error: "Name is required"})
+    .trim()
+    .min(3,{message:"Name must be atleast of 3 chars."})
+    .max(255,{message:"Name must not be more than 255 character"}),
+})
+
+export {signupSchema};
--- server/router/route.js
+++ server/router/route.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
 import UserController from "../controllers/user.controller.js"
+import { validate } from "../middleware/user.middleware.js";
+import { signupSchema } from "../Validators/user.validator.js";
 
 const router = Router();
 router.route('/').get(UserController.home)
-router.route('/register').post(UserController.register)
+router.route('/register').post( validate(signupSchema) ,UserController.register)
 router.route('/login').post(UserController.login)
 
 export default router //(module js)

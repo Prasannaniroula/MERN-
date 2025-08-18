f1569bf73812c142d40ea5e62910e4b37274f000
Prasannaniroula <Prasannaniroula5@gmail.com>
2025-08-18 21:33:43 +0545

    add CORS support to server and update package dependencies

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import route from "./router/route.js"
 import DB from "./utils/db.utils.js"
+import cors from "cors"
 import { errorMiddleware } from "./middleware/ApiError.middleware.js";
 import {contactRouter} from "./router/Contact.router.js";
 const app = express();
 const PORT = 8000;
+
+
+const corsOptions = {
+    origin:"http://localhost:5173",
+    methods:"GET, POST, PUT,DELETE",
+    credentials:true
+}
+ app.use(cors(corsOptions));
 
 app.use(express.json())
 
